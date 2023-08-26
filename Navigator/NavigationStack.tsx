c26a953a29a1726b78c82348f1301112e26122a7
Pearl Dere <82610538+Pearl200@users.noreply.github.com>
2023-08-26 12:07:00 +0000

    Update NavigationStack.tsx

--- Navigator/NavigationStack.tsx
+++ Navigator/NavigationStack.tsx
@@ -3,7 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./screens/LoginScreen";
 import HomeScreen from "./screens/HomeScreen";
-import ProfileScreen from "./screens/ProfileScreen";
+import ProfileScreen from "./screens/PostScreen";
 
 const Stack = createNativeStackNavigator();
 
